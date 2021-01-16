b2be104edfedc42b290dc089708a32e3509f1405
Gabriel Huber <mail@gabrielhuber.at>
2021-01-17 00:42:16 +0100

    Improve code accuracy

--- l4d2query/serverdetails.py
+++ l4d2query/serverdetails.py
@@ -5,6 +5,7 @@ import enum
 
 import io
 import socket
+import time
 
 from l4d2query.byteio import ByteReader, ByteWriter
 
@@ -14,9 +15,15 @@ DEFAULT_ENCODING = "utf-8"
 class TokenType(enum.IntEnum):
     OBJECT = 0
     STRING = 1
-    UINT32 = 2
-    INT32 = 3
+    INT32 = 2
+    FLOAT = 3
+    PTR = 4
+    WSTRING = 5
+    COLOR = 6
     UINT64 = 7
+    COMPILED_INT_BYTE = 8
+    COMPILED_INT_0 = 9
+    COMPILED_INT_1 = 10
     END = 11
 
 @dataclass
@@ -34,11 +41,10 @@ def defaultdataclass(cls):
 @defaultdataclass
 class TokenPacket:
     header: bytes
-    type: int
-    unknown_1: int
+    engine_build: int
     payload_size: int
-    unknown_2: int
     data: dict
+    binary_data: bytes
 
 
 def read_token(reader):
@@ -56,10 +62,12 @@ def read_token(reader):
             value[token.key] = token.value
     elif token_type == TokenType.STRING:
         value = reader.read_cstring()
-    elif token_type == TokenType.UINT32:
-        value = reader.read_uint32()
     elif token_type == TokenType.INT32:
         value = reader.read_int32()
+    elif token_type == TokenType.FLOAT:
+        value = reader.read_float()
+    elif token_type == TokenType.PTR:
+        value = reader.read_uint32()
     elif token_type == TokenType.UINT64:
         value = reader.read_uint64()
     else:
@@ -76,10 +84,12 @@ def write_token(writer, token_type, name, value):
         pass
     elif token_type == TokenType.STRING:
         writer.write_cstring(value)
-    elif token_type == TokenType.UINT32:
-        writer.write_uint32(value)
     elif token_type == TokenType.INT32:
         writer.write_int32(value)
+    elif token_type == TokenType.FLOAT:
+        writer.write_float(value)
+    elif token_type == TokenType.PTR:
+        writer.write_uint32(value)
     elif token_type == TokenType.UINT64:
         writer.write_uint64(value)
     else:
@@ -87,37 +97,36 @@ def write_token(writer, token_type, name, value):
 
 def decode_tokenpacket(packet_data, encoding):
     stream = io.BytesIO(packet_data)
-    reader = ByteReader(stream, endian=">", encoding=encoding)
+    reader = ByteReader(stream, endian="<", encoding=encoding)
     packet = TokenPacket()
     packet.header = reader.read(8)
-    packet.type = reader.read_uint16()
-    packet.unknown_1 = reader.read_uint16()
-    packet.payload_size = reader.read_uint8()
-    packet.unknown_2 = reader.read_uint8()
+    packet.engine_build = reader.read_uint32()
+    packet.payload_size = reader.read_uint32()
+    reader.endian = ">" # yes, this is an endian switch in the middle of the packet
     root_token = read_token(reader)
     packet.data = root_token.value
+    reader.read_uint8()
+    binary_size = reader.read_uint32()
+    packet.binary_data = reader.read(binary_size)
     return packet
 
 def construct_serverdetails(timestamp, pingxuid):
     stream = io.BytesIO()
-    writer = ByteWriter(stream, endian=">", encoding="utf-8")
+    writer = ByteWriter(stream, endian="<", encoding="utf-8")
     writer.write(b"\xff\xff\xff\xff\x00\x00\x00\x00") # header
-    writer.write_uint16(0xa308) # type
-    writer.write_uint16(0) # unknown 1
-    writer.write_uint8(60) # payload_size
-    writer.write_uint8(0) # unknown 2
-    write_token(writer, TokenType.OBJECT, "", None)
+    writer.write_uint32(2211) # engine_build
+    writer.write_uint32(60) # payload_size
+    writer.endian = ">"
     write_token(writer, TokenType.OBJECT, "InetSearchServerDetails", None)
-    write_token(writer, TokenType.INT32, "timestamp", timestamp)
+    write_token(writer, TokenType.FLOAT, "timestamp", timestamp)
     write_token(writer, TokenType.UINT64, "pingxuid", pingxuid)
     write_token(writer, TokenType.END, None, None)
     write_token(writer, TokenType.END, None, None)
-    writer.write(b"\x00\x00\x00\x00") # footer or padding, idk
-
+    writer.write_uint32(0) # binary size
     return bytes(stream.getbuffer())
 
 def query_serverdetails(addr, timeout=DEFAULT_TIMEOUT, encoding=DEFAULT_ENCODING):
-    request_data = construct_serverdetails(timestamp=0, pingxuid=0)
+    request_data = construct_serverdetails(timestamp=time.time(), pingxuid=0)
     s = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
     s.settimeout(timeout)
     s.sendto(request_data, addr)
